cd92e96955f7fe27b7f0ee8d2d27ab8337f66082
Aston Aakerman <aston.akerman.coio@outlook.com>
2024-04-26 09:45:10 +0200

    chore: Adding WebScraper ascii art

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from webscraper import scrape_website
 
 def main(args):
     output_directory = create_output_path(args.url, args.output)
+    print('''
+888       888          888       .d8888b.                                                     
+888   o   888          888      d88P  Y88b                                                    
+888  d8b  888          888      Y88b.                                                         
+888 d888b 888  .d88b.  88888b.   "Y888b.    .d8888b 888d888 8888b.  88888b.   .d88b.  888d888 
+888d88888b888 d8P  Y8b 888 "88b     "Y88b. d88P"    888P"      "88b 888 "88b d8P  Y8b 888P"   
+88888P Y88888 88888888 888  888       "888 888      888    .d888888 888  888 88888888 888     
+8888P   Y8888 Y8b.     888 d88P Y88b  d88P Y88b.    888    888  888 888 d88P Y8b.     888     
+888P     Y888  "Y8888  88888P"   "Y8888P"   "Y8888P 888    "Y888888 88888P"   "Y8888  888     
+                                                                    888                       
+                                                                    888                       
+                                                                    888                       
+          ''')
     scrape_website(args.url, output_directory)
 
 def create_output_path(url, output_path):
